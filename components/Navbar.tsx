a5d645b3eb39f9de1192f7cf566155ee7e97eef0
i-amdash <ashimidoyin@yahoo.com>
2024-08-13 06:22:41 +0100

    added overflow hidden to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import { CiShop } from "react-icons/ci";
 const Navbar = () => {
   return (
     <header className="">
-      <nav className="flex max-w-fit md:min-w-[70vw] text-white lg:min-w-fit fixed z-[5000] top-5 lg:top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-2 lg:space-x-4"
+      <nav className="flex max-w-fit md:min-w-[70vw] text-white lg:min-w-fit fixed z-[5000] top-5 lg:top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-1 overflow-x-hidden lg:space-x-4"
       style={{
         backdropFilter: "blur(16px) saturate(180%)",
         backgroundColor: "rgba(114,85,60, 0.25)",
